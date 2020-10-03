33b0b69bea0e39a6b14dd99a06fa3c0b3a17a5ea
Ilya <icasin478@gmail.com>
2020-10-03 20:15:09 +0800

    Add function for input

--- src/computing.c
+++ src/computing.c
@@ -1,5 +1,25 @@
 #include "computing.h"
 #include <stdio.h>
+#include <string.h>
+
+int stringing(             //
+        int count_words)   // function for enter data and check quantity symbols
+{                          //
+    int i = 0;             // init var for clear clipboard
+    char* str;             // init string for data
+    int count_symbols = 0; // init var for count symbols in the text
+    char array[count_words * 16];                         // create char array
+    str = array;                                          // array --> str
+    printf("Please use a space after punctuation marks"); //
+    printf("\nEnter u text ==> ");                        //
+    scanf("\n%d", &i);                            // scan for clean clipboard
+    while ((*str++ = getchar()) != '\n') {        // enter data
+        count_symbols++;                          // quantity symbols ++;
+        *str = '\0';                              // add last mark string
+    }                                             //
+    computing(array, count_words, count_symbols); // void function
+    return 0;                                     //
+}
 
 void print() // function for print start text
 {            //
