a2dd8ab54580c51cf2cf4449e70e597ccde46c86
He3k-Notebook <Nickolablaniy@mail.ru>
2020-10-05 16:38:54 +0700

    Add new enter

--- src/computing.h
+++ src/computing.h
@@ -2,6 +2,9 @@
 #define COMPUTING_H
 #include <stdio.h>
 
+int sort(char* k1, char* k2);
 int stringing(int count_words);
+char computing(char array[][16],int count_words);
 void print();
+int stringing(int count_words);
 #endif
