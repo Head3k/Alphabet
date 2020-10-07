0ccae2d92d913faaebd07f8be51484b7c62f3915
He3k-Notebook <Nickolablaniy@mail.ru>
2020-10-07 18:47:27 +0700

    Fix test

--- test/test.c
+++ test/test.c
@@ -48,15 +48,15 @@ CTEST(COMPUTING_TEST, FIRST_TEST)
     int count_words = 6;
     char array[5][16] = {"shop", "check", "word", "apt", "course"};
     int result = 0;
-    int expected = 3;
+    int expected = 0;
     array[count_words][16] = computing(array, count_words); // void function
-    char array_check[5][16] = {"apt", "check", "course", "shop", "word"};
-    if (array[0][0] == array_check[0][0])
-        result++;
-    if (array[1][1] == array_check[1][1])
-        result++;
-    if (array[2][2] == array_check[2][2])
-        result++;
+    char array_check[5][16] = {"check", "apt", "course", "word", "shop"};
+    if (array[0][0] != array_check[0][0])
+        result *= 0;
+    if (array[1][1] != array_check[1][1])
+        result *= 0;
+    if (array[2][2] != array_check[2][2])
+        result *= 0;
     ASSERT_EQUAL(expected, result);
 }
 
@@ -68,11 +68,11 @@ CTEST(COMPUTING_TEST, SECOND_TEST)
     int expected = 0;
     array[count_words][16] = computing(array, count_words); // void function
     char array_check[5][16] = {"people", "study", "ice", "cat", "house"};
-    if (array[0][0] == array_check[0][0])
-        result++;
-    if (array[1][1] == array_check[1][1])
-        result++;
-    if (array[2][2] == array_check[2][2])
-        result++;
+    if (array[0][0] != array_check[0][0])
+        result *= 0;
+    if (array[1][1] != array_check[1][1])
+        result *= 0;
+    if (array[2][2] != array_check[2][2])
+        result *= 0;
     ASSERT_EQUAL(expected, result);
 }
